c767ca496e79bb176ac3874516252e25eff9a614
Eddy Izakson <eddyizakson@gmail.com>
2019-05-14 12:33:18 +0300

    commit

---
+++ NavMesh Exercise/Assets/Target.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Target : MonoBehaviour
+{
+    NavMeshAgent[] meshAgents;
+    Transform targetTransform; 
+    
+        // Start is called before the first frame update
+    void Start()
+    {
+        meshAgents = FindObjectsOfType(typeof(NavMeshAgent)) as NavMeshAgent[];
+        targetTransform = GetComponent<Transform>();
+    }
+
+    void UpdateTarget(Vector3 pos)
+    {
+        foreach (NavMeshAgent navMeshAgent in meshAgents)
+        {
+            navMeshAgent.destination = pos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out raycastHit))
+            {
+                Vector3 newPos = raycastHit.point;
+                UpdateTarget(newPos);
+                targetTransform.position = newPos;
+            }
+        }
+    }
+}
